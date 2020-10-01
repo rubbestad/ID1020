65fd533ffd47032f6363c8c8d4ba21ac6b19234b
rubbestad <54176579+rubbestad@users.noreply.github.com>
2020-10-01 18:14:43 +0200

    Update L1A5.java

--- Fundamentals/L1A5.java
+++ Fundamentals/L1A5.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 /*
  *  --------------------------------------------------------------
- * 	Implement a generalized queue which allows the user to remove
+ *  Implement a generalized queue which allows the user to remove
  *  the kth element from the queue. Assume the most recently added
  *  element has index 1. You should print the content of the list
  *  after each insertion/deletion of an element.
